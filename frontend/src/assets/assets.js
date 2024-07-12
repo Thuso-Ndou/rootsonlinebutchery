f84f4d6efcdbbf1f64676906b3f937a2e60fa677
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-07-13 00:00:15 +0200

    Menu Icons

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,14 @@
 import logo from './transparent-logo.webp';
 import search from './searchRed.png';
 import cart from './cart.png';
+import chicken_menu from './chicken_menu.jpg';
+import spicies_menu from './spice.jpg';
+import veg_menu from './veg.jpg';
+import beef_menu from './beef.jpg';
+import fish_menu from './fish.jpg';
+import lamp_menu from './lamp.jpg';
+import pork_menu from './pork.jpg';
+import wors_menu from './wors.jpg';
 
 export const assets = {
     logo,
@@ -8,6 +16,37 @@ export const assets = {
     cart,
 }
 
-export const menu = {
-    logo,
-}
+export const menu_list = [
+    {
+        menu_name: 'Chicken',
+        menu_image: chicken_menu,
+    },
+    {
+        menu_name: 'Beef',
+        menu_image: beef_menu,
+    },
+    {
+        menu_name: 'Spices',
+        menu_image: spicies_menu,
+    },
+    {
+        menu_name: 'Vegetables',
+        menu_image: veg_menu,
+    },
+    {
+        menu_name: 'Pork',
+        menu_image: pork_menu,
+    },
+    {
+        menu_name: 'Wors',
+        menu_image: wors_menu,
+    },
+    {
+        menu_name: 'Lamp',
+        menu_image: lamp_menu,
+    },
+    {
+        menu_name: 'Fish',
+        menu_image: fish_menu,
+    },
+]
---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,8 @@
+//import React from 'react'
+import './ExploreMenu.css';
+
+export default function ExploreMenu() {
+  return (
+    <div>ExploreMenu</div>
+  )
+}
