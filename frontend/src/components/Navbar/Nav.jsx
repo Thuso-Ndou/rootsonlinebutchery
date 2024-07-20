60abc69c72470ddb870474061dd56c9f95714fba
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-07-20 04:41:33 +0200

    cart

--- frontend/src/components/Navbar/Nav.jsx
+++ frontend/src/components/Navbar/Nav.jsx
@@ -10,19 +10,19 @@ export default function Nav({setShowLogin}) {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt=""  className='logo'/>
+        <Link to='/' onClick={()=>setMenu("home")}><img src={assets.logo} alt=""  className='logo'/></Link>
 
         <ul className='navbar-menu'>
             <Link to='/'  onClick={()=>setMenu("home")} className={menu==='home'?'active':''}>Home</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==='menu'?'active':''}>Menu</a>
-            <a href='#track-order' onClick={()=>setMenu("track-order")}  className={menu==='track-order'?'active':''}>Track Order</a>
+            <a href='#track-order' onClick={()=>setMenu("track-order")}  className={menu==='track-order'?'active':''}>Track Orders</a>
             <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==='contact-us'?'active':''}>Contact Us</a>
         </ul>
 
         <div className='navbar-right'>
             <img src={assets.search} alt="search" />
             <div className='navbar-search-icon'>
-                <img src={assets.cart} alt="cart" />
+                <Link to='/cart'><img src={assets.cart} alt="cart" /></Link>
                 <div className='dot'></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Sign In</button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,65 @@
 //import React from 'react'
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
 import './Cart.css';
 
 export default function Cart() {
+
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContext)
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <div>
+                <div className='cart-items-title cart-items-item'>
+                <img src={item.image} alt=''/>
+                <p>{item.name}</p>
+                <p>R{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>R{item.price*cartItems[item._id]}</p>
+                <p onClick={()=>removeFromCart(item._id)} className='cancel'>-</p>
+              </div>
+              <hr/>
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
