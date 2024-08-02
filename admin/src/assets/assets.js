292a092e0b4ec35aa5f23a0be9cee6a88b18423e
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-02 06:50:24 +0200

    done with add sidebar styling

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,7 @@
 import parcel from './parcel-32.png';
 import add from './red-add-32.png';
 import order from './orderRed.png';
-import upload from './upload-64.png';
+import upload from './upload-60.png';
 import profile from './profile-48.png';
 import logo from './transparent-logo.webp';
 
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -19,23 +19,28 @@ const Add = () => {
             </div>
             <div className="add-product-des flex-col">
                 <p>Product Description</p>
-                <textarea name="description" rows='6' placeholder='Write content here' required></textarea>
+                <textarea name="description" rows='6' placeholder='Write product description here' required></textarea>
             </div>
             <div className="add-category-price">
                 <div className="add-category flex-col">
                     <p>Product Category</p>
                     <select name="category">
-                        <option value="Chicken"></option>
-                        <option value="Beef"></option>
-                        <option value="Spices"></option>
-                        <option value="Fruits & Veg"></option>
-                        <option value="Pork"></option>
-                        <option value="Lamb"></option>
-                        <option value="Fish"></option>
-                        <option value="Other"></option>
+                        <option value="Chicken">Chicken</option>
+                        <option value="Beef">Beef</option>
+                        <option value="Spices">Spices</option>
+                        <option value="Fruits & Veg">Fruits & Veg</option>
+                        <option value="Pork">Pork</option>
+                        <option value="Lamb">Lamb</option>
+                        <option value="Fish">Fish</option>
+                        <option value="Other">Other</option>
                     </select>
                 </div>
+                <div className="add-price flex-col">
+                    <p>Product Price</p>
+                    <input type="Number" name='price' placeholder='R40'/>
+                </div>
             </div>
+            <button type='submit' className='add-button'>ADD</button>
         </form>
     </div>
   )
