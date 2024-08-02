17d7d489ea5eeb32e098e1ca4ec3923cea1e80b8
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-02 05:03:30 +0200

    admin sidebar

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,5 +1,5 @@
 import parcel from './parcel-32.png';
-import add from './icons8-add-32.png';
+import add from './red-add-32.png';
 import order from './orderRed.png';
 import upload from './cloud-upload-32.png';
 import profile from './profile-48.png';
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
 //import React from 'react'
 import './Sidebar.css';
+import {assets} from '../../assets/assets';
 
 const Sidebar = () => {
   return (
-    <div>
-
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img src={assets.add} alt="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order} alt="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.parcel} alt="" />
+                <p>Orders</p>
+            </div>
+        </div>
     </div>
   )
 }
