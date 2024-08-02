81270a1271a5884a2f8999911d398b7ea14c5700
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-02 04:20:47 +0200

    admin components

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,11 +3,13 @@ import add from './icons8-add-32.png';
 import order from './orderRed.png';
 import upload from './cloud-upload-32.png';
 import profile from './profile-48.png';
+import logo from './transparent-logo.webp';
 
 export const assets ={
     parcel,
     add,
     order,
     upload,
-    profile
+    profile,
+    logo
 }
