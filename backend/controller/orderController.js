de7d2621648bea840ba70407fc2d74c2cf017352
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-08 21:01:02 +0200

    verify order success

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -61,4 +61,21 @@ const placeOrder = async (req,res) => {
     }
 }
 
-export {placeOrder};
+const verifyOrder = async(req, res) => {
+    const {orderId,success} = req.body;
+    try {
+        if(success=="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"Payment successful"})
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false,message:"Payment failed"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"An error occurred while processing the order"});
+    }
+}
+
+export {placeOrder, verifyOrder};
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder } from '../controller/orderController.js';
+import { placeOrder, verifyOrder } from '../controller/orderController.js';
 
 const orderRouter = express.Router();
 
 // end point
 orderRouter.post('/place', authMiddleware, placeOrder);
+orderRouter.post("/verify", verifyOrder);
 
 export default orderRouter;
