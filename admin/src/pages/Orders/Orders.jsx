516c4547940fbdf4b58d2e634faa659a7a437cb6
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-09 00:49:27 +0200

    frontend and Api are working

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,32 @@
 //import React from 'react'
+import { useState } from 'react';
 import './Orders.css';
+import axios from 'axios';
+import {toast} from 'react-toastify';
+import { useEffect } from 'react';
 
 const Orders = ({url}) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async() => {
+    const response = await axios.get(url+"/api/order/list");
+    if(response.data.success){
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }
+    else{
+      toast.error("Error");
+    }
+  }
+
+  useEffect(()=>{
+    fetchOrders();
+  },[])
+
   return (
-    <div>Orders</div>
+    <div>
+
+    </div>
   )
 }
 
