c05c51a0bb44813b9649fb701a397c5c377d10d6
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-08-08 21:39:08 +0200

    payment verification working

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,8 +1,9 @@
 //mport React from 'react'
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import './Verify.css';
-import {useSearchParams} from 'react-router-dom';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 import {StoreContext} from '../../context/StoreContext';
+import axios from 'axios';
 
 function Verify() {
 
@@ -10,6 +11,21 @@ function Verify() {
     const success = searchText.get("success");
     const orderId = searchText.get("orderId");
     const {url} = useContext(StoreContext);
+    const navigate = useNavigate();
+
+    const verifyPayment = async () => {
+        const response = await axios.post(url+"/api/order/verify",{success, orderId});
+        if(response.data.success){
+            navigate("/trackOrders");
+        }
+        else{
+            navigate("/");
+        }
+    }
+
+    useEffect(()=>{
+        verifyPayment();
+    },[])
 
   return (
     <div className='verify'>
