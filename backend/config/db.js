1b447da262926ca5888799c31a1649b894422def
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2025-07-21 18:58:31 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connDB = async () => {
-    await mongoose.connect('mongodb+srv://thusondou:thusoND21@cluster0.n3anfx2.mongodb.net/rootsonlinebutchery').then(()=>console.log("Connected to MongoDB"));
+    await mongoose.connect('mongodb+srv://**********@cluster0.n3anfx2.mongodb.net/rootsonlinebutchery').then(()=>console.log("Connected to MongoDB"));
 }
